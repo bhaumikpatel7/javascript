0a2eca745c03bdce7e2fd8adf647ba90051a72b3
Bhaumik Patel <72443943+bhaumikpatel7@users.noreply.github.com>
2023-10-07 03:48:38 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //tream all JS code as newer version
+
+// alert(3+3) it will give you error becoz we are running in the node js not a browser
+
+// console.log(3+3)
+
+let name = "bhaumik"
+let age = 18
+let isLoggedIn = true
+let state; //undefiend
+let state1 = null;
+
+//number  => 2 ** 53
+//bigint--used in trading in stockmarket
+//string => " " or ''
+//boolean => true false
+//null- it is a standalone value
+//undefiend => 
+//symbol => unique
+// object 
+
+console.log(typeof"bhaumik")
+console.log(typeof null). //op - object
+console.log(typeof undefined) // op - undefined
