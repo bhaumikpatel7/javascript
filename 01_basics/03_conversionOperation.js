defef8880cb5b7116ee60b26eea4a008d97c62f3
Bhaumik Patel <72443943+bhaumikpatel7@users.noreply.github.com>
2023-10-09 00:54:07 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,8 +25,36 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+//+++++++++++++++++++++Operation+++++++++++++++++++++++++++++++++++++
+
+let value = 3
+let negValue = -value
+// console.log(value)
+
+let str1 = "Hello"
+let str2 = " Bhaumik"
+
+let str3 = str1 + str2;
+// console.log(str3)
+
+// console.log("1"+2); //12
+// console.log("2"+1); //21
+// console.log("1"+2 +2);  // 122
+// console.log(1+2 +"2");  //32
+
+// console.log(+true)// 1
+
+let gameCounter = 200
+gameCounter++
+console.log(gameCounter)
+
+
+
+
 
 
 
