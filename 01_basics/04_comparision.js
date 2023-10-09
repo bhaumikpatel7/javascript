51cc0a3a1998c3c57a3a39c1e6143ff2cdbd33f6
Bhaumik Patel <72443943+bhaumikpatel7@users.noreply.github.com>
2023-10-09 01:20:06 +0000

    data types

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+console.log("2" > 1);
+
+console.log(null > 0); // null is a 0 value
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === check data type as well
+
+console.log("2" == 2);
+console.log();
