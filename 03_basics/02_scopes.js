4933cce937d9962a1ce53494078c9a858b6b0477
Bhaumik Patel <72443943+bhaumikpatel7@users.noreply.github.com>
2023-10-12 02:29:59 +0000

    mini hoisting

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,61 @@
+// var c = 300;
+let a = 300;
+
+if (true) {
+
+    let a = 10;
+    const b = 20;
+    // var c = 30;
+
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Bhaumik"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    // console.log(website);
+
+    two()
+}
+
+one()
+
+if (true) {
+
+    const username = "bhaumik"
+    if (username === "bhaumik") {
+
+        const website = " youtube"
+        console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+
+//+++++++++++++++++++++++++++++++interesting ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// addone(5) does not gives error
+function addone(num){
+    return num+1;
+}
+addone(5)
+
+
+//addTwo(5) // gives you error
+const addTwo = function(num){
+    return num +2;
+}
+addTwo(5)
